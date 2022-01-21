788e909bcb4627be3b29a55e0b198c56b45e733b
wambatch <39588962+wambatch@users.noreply.github.com>
2022-01-21 15:02:53 +0700

    Update soundmanager2.js

--- soundmanager2.js
+++ soundmanager2.js
@@ -107,9 +107,7 @@ function SoundManager(smURL, smID) {
     onplay: null,           // callback for "play" start
     onpause: null,          // callback for "pause"
     onresume: null,         // callback for "resume" (pause toggle)
-    whileplaying: function() {
-    console.log('ID = '+this.id+'<br/>URL = '+this.url+'<br/>Time = '+s.duration+'<br/>Time2 = '+duration+'<br/>DONE?');
-  },     // callback during play (position update)
+    whileplaying: null,     // callback during play (position update)
     onposition: null,       // object containing times and function callbacks for positions of interest
     onstop: null,           // callback for "user stop"
     onfinish: null,         // callback function for "sound finished playing"
